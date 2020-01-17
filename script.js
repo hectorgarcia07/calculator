ec37f34d372a0357dbd7d8464ceea33acd5225ce
Hector Garcia <hector.garcia07@utrgv.edu>
2020-01-17 13:13:11 -0600

    added basic javascript logic for buttons, and fixed display

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+var max_len = 15;
+var screen = document.getElementById("screen-num");
+
+//set each button to have an event
+function set_buttn_event()
+{
+    let buttons = document.getElementsByClassName("buttons");
+    //give each button an event
+    for(let i = 0; i < buttons.length; i++)
+    {
+        buttons[i].addEventListener("click", function(){
+            handle_buttn_event(this);
+        });
+    }
+}
+
+//determines what button was pressed and determines its
+//action
+function handle_buttn_event(obj){
+    let content = obj.textContent;
+    
+    //check if it is a number
+    if(!isNaN(content))
+    {
+        console.log(content);
+    }
+    else if(content === "Clr")
+    {
+        console.log(content);
+    }
+    else if(content === "Del")
+    {
+        console.log(content);
+    }
+    else if(content === "/")
+    {
+        console.log(content);
+    }
+    else if(content === "*")
+    {
+        console.log(content);
+    }
+    else if(content === "+")
+    {
+        console.log(content);
+    }
+    else if(content === "-")
+    {
+        console.log(content);
+    }
+    else if(content === "=")
+    {
+        console.log(content);
+    }
+    else if(content === ".")
+    {
+        console.log(content);
+    }
+}
+
 function add(numOne, numTwo){
     return numOne + numTwo;
 }
@@ -26,3 +86,5 @@ function operate(operator, numOne, numTwo)
         mul(numOne, numTwo);
 
 }
+
+set_buttn_event();
