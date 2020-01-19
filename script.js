470d956f3363fdc0a29f38be59db18a8731283b9
Hector Garcia <hector.garcia07@utrgv.edu>
2020-01-18 21:58:30 -0600

    updated equals and operator javascript logic

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var max_len = 15;
+var cur_len = 0;
+var displayVal = "";
+var operator = "";
 var screen = document.getElementById("screen-num");
 
 //set each button to have an event
@@ -21,34 +24,34 @@ function handle_buttn_event(obj){
     
     //check if it is a number
     if(!isNaN(content))
+        updateDisplay(content);
+
+    switch(content)
     {
-        console.log(content);
+        //calculate the result of the expression
+        case "=":
+            console.log(screen.textContent);
+            displayVal = operate(operator, displayVal, screen.textContent);
+            screen.innerHTML = displayVal;
+            console.log(displayVal);
+            break;
+        //if it's an operator, update the operator
+        //and update the screen size
+        case "/":
+        case "*":
+        case "+":
+        case "-":
+            operator = content;
+            displayVal = screen.innerHTML;
+            cur_len = 0;
+            screen.innerHTML = "0";
+            break;
     }
-    else if(content === "Clr")
+     if(content === "Clr")
     {
         console.log(content);
     }
     else if(content === "Del")
-    {
-        console.log(content);
-    }
-    else if(content === "/")
-    {
-        console.log(content);
-    }
-    else if(content === "*")
-    {
-        console.log(content);
-    }
-    else if(content === "+")
-    {
-        console.log(content);
-    }
-    else if(content === "-")
-    {
-        console.log(content);
-    }
-    else if(content === "=")
     {
         console.log(content);
     }
@@ -58,8 +61,20 @@ function handle_buttn_event(obj){
     }
 }
 
+//function will update the display.
+function updateDisplay(num)
+{
+    if(cur_len < max_len)
+    {
+        if(cur_len === 0)
+            screen.innerHTML = num;
+        else
+            screen.insertAdjacentHTML('beforeend', num);
+        cur_len++;
+    }
+}
 function add(numOne, numTwo){
-    return numOne + numTwo;
+    return +numOne + +numTwo;
 }
 
 function sub(numOne, numTwo){
@@ -74,17 +89,23 @@ function mul(numOne, numTwo){
     return numOne * numTwo;
 }
 
+//will caclulate and return anwer based on user
+//selected operator
 function operate(operator, numOne, numTwo)
 {
-    if(operator === "+")
-        add(numOne, numTwo);
-    else if (operator === "-")
-        sub(numOne, numTwo);
-    else if (operator === "/")
-        divide(numOne, numTwo);
-    else if(operator === "*")
-        mul(numOne, numTwo);
+    if(numOne.length === 0 && numTwo.length === 0)
+        return "0";
+    else if(numOne.length > 0 && numTwo.length === 0)
+        return numOne;
 
+    if(operator === "+")
+        return add(numOne, numTwo);
+    else if (operator === "-")
+        return sub(numOne, numTwo);
+    else if (operator === "/")
+        return divide(numOne, numTwo);
+    else if(operator === "*")
+        return mul(numOne, numTwo);
 }
 
 set_buttn_event();
